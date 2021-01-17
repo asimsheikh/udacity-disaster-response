74af1834cd34777ae5892e77c264b82b19062b93
asheikh <asim.sardar.sheikh@gmail.com>
2021-01-17 15:56:34 +0100

    Updating ETL script

--- data/process_data.py
+++ data/process_data.py
@@ -1,8 +1,55 @@
+# import libraries
+
 import sys
 
+import pandas as pd
+from sqlalchemy import create_engine
 
-def load_data(messages_filepath, categories_filepath):
-    pass
+
+def load_data(messages_filepath: str, categories_filepath: str) -> pd.DataFrame:
+    # load dataset
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+
+    # merge datasets
+    df = pd.merge(messages, categories, on="id")
+
+    # create a dataframe of the 36 individual category columns
+    categories = categories["categories"].str.split(";", expand=True)
+
+    # select the first row of the categories dataframe
+    row = categories.iloc[0].apply(lambda text: text[:-2])
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything
+    # up to the second to last character of each string with slicing
+    category_colnames = categories.iloc[0].apply(lambda text: text[:-2])
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda text: text.split("-")[-1])
+
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    df.drop("categories", axis=1, inplace=True)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis="columns")
+
+    # check number of duplicates
+    duplicates_pre = df.duplicated().sum()
+    # drop duplicates
+    filt = df.duplicated()
+    df = df[~filt]
+    # check number of duplicates
+    duplicates_post = df.duplicated().sum()
+
+    engine = create_engine("sqlite:///messages.db")
+    df.to_sql("messages", engine, index=False)
 
 
 def clean_data(df):
@@ -10,7 +57,7 @@ def clean_data(df):
 
 
 def save_data(df, database_filename):
-    pass  
+    pass
 
 
 def main():
@@ -18,26 +65,31 @@ def main():
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
 
-        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
-              .format(messages_filepath, categories_filepath))
+        print(
+            "Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}".format(
+                messages_filepath, categories_filepath
+            )
+        )
         df = load_data(messages_filepath, categories_filepath)
 
-        print('Cleaning data...')
+        print("Cleaning data...")
         df = clean_data(df)
-        
-        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+
+        print("Saving data...\n    DATABASE: {}".format(database_filepath))
         save_data(df, database_filepath)
-        
-        print('Cleaned data saved to database!')
-    
+
+        print("Cleaned data saved to database!")
+
     else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
-              'DisasterResponse.db')
+        print(
+            "Please provide the filepaths of the messages and categories "
+            "datasets as the first and second argument respectively, as "
+            "well as the filepath of the database to save the cleaned data "
+            "to as the third argument. \n\nExample: python process_data.py "
+            "disaster_messages.csv disaster_categories.csv "
+            "DisasterResponse.db"
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
